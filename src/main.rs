d7fceaf9b731251cdbe8642c716dfaa3a697349a
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-06-22 19:37:48 +0200

    feat(main.rs): :thread: Demo server as in axum docs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let app = axum::Router::new().route("/", axum::routing::get(|| async {"hey"}));
+
+    axum::Server::bind(&"127.0.0.1:9999".parse().unwrap())
+      .serve(app.into_make_service())
+      .await
+      .unwrap();
 }
