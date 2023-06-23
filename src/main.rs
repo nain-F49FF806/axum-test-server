f3a58597fc05fe5353140e764d532446ff10000e
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-06-23 14:13:40 +0200

    refactor(main.rs): use local names for Router, get

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
+use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    let app = axum::Router::new().route("/", axum::routing::get(|| async {"hey"}));
+    let app = Router::new().route("/", get(|| async {"hey"}));
 
     axum::Server::bind(&"127.0.0.1:9999".parse().unwrap())
       .serve(app.into_make_service())
