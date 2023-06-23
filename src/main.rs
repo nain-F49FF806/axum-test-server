f519e8a796a5fe56f9b969df39f6bb1fd8b55540
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-06-23 14:17:55 +0200

    refactor(main.rs): pull out handler function

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    let app = Router::new().route("/", get(|| async {"hey"}));
+    let app = Router::new().route("/", get(handle_get));
 
     axum::Server::bind(&"127.0.0.1:9999".parse().unwrap())
       .serve(app.into_make_service())
       .await
       .unwrap();
+}
+
+async fn handle_get() -> String  {
+    "hey".to_owned()
 }
