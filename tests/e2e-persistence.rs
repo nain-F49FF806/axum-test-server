babe8115e875b4d7616b7186abb73ce0cafc0a49
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-07-19 12:49:10 +0200

    test(integration): create e2e-persistence.rs, test forward route available

---
+++ tests/e2e-persistence.rs
@@ -0,0 +1,23 @@
+// Copyright 2023 Naian G.
+// SPDX-License-Identifier: Apache-2.0
+
+use xum_test_server::didcomm_types::{ForwardMsg, StatusMsg, TypeURI};
+
+const BASE_URL: &str = "http://localhost:7999";
+
+// Test variables
+const RECIPIENT_KEY: &str = "Alice";
+const HELLO_ALICE: &str = "Hello Alice!";
+
+
+#[test]
+fn test_forward_message() {
+    let client = reqwest::blocking::Client::new();
+    let endpoint = format!("{BASE_URL}/forward");
+    let forward_message = ForwardMsg::new(
+        RECIPIENT_KEY,
+        HELLO_ALICE
+    );
+    let res = client.post(endpoint).json(&forward_message).send().unwrap();
+    res.error_for_status().unwrap();
+}
