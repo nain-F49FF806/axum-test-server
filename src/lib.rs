d305116b5a812c0435cbf00469f87f47856b87a0
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-07-19 12:17:50 +0200

    build(deps): Refactor main to use self package as library

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright 2023 Naian G.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod logging;
+pub mod router;
+pub mod routes;
+pub mod server;
+pub mod database;
+pub mod didcomm_types;
+pub mod storage;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
-mod logging;
-mod router;
-mod routes;
-mod server;
-mod database;
+
 use log::info;
-use server::run_server;
+use xum_test_server::logging;
+use xum_test_server::server;
 
 #[tokio::main]
 async fn main() {
+    let _ = dotenvy::dotenv();
     logging::init_logger();
     info!("Hello, world!");
-    let _pool = database::get_db_pool().await;
-    run_server().await;
+    server::run_server().await;
 }
