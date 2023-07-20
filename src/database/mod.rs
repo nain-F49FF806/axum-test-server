48ab9b996722298f88d2a5ae3710a5794e9d5d3d
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-07-20 16:46:08 +0200

    build(deps): Remove dependency on cfg_if. Also emit compilation error if more than one backend db selected

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,18 +1,18 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
-use cfg_if::cfg_if;
 
-cfg_if! {
-    if #[cfg(feature = "AnyDB")] {
-        mod any;
-        pub use any::get_db_pool;
-    }
-    else if #[cfg(feature = "PostgresqlDB")] {
-        mod postgres;
-        pub use postgres::get_db_pool;
-    }
-    else if #[cfg(feature = "MysqlDB")] {
-        mod mysql;
-        pub use mysql::get_db_pool;
-    }
-}
+#[cfg(feature = "any_db")] 
+mod any;
+#[cfg(feature = "any_db")] 
+pub use any::get_db_pool;
+
+#[cfg(feature = "postgres_db")] 
+mod postgres;
+#[cfg(feature = "postgres_db")] 
+pub use postgres::get_db_pool;
+
+#[cfg(feature = "mysql_db")] 
+mod mysql;
+#[cfg(feature = "mysql_db")] 
+pub use mysql::get_db_pool;
+
--- src/storage.rs
+++ src/storage.rs
@@ -3,22 +3,24 @@
 
 use crate::database::get_db_pool;
 use crate::didcomm_types::ForwardMsg;
-use cfg_if::cfg_if;
+// use async_trait::async_trait;
 
-cfg_if! {
-    if #[cfg(feature = "AnyDB")] {
-        pub struct Storage {
-            pool: sqlx::AnyPool,
-        }
-    } else if #[cfg(feature = "PostgresqlDB")] {
-        pub struct Storage {
-            pool: sqlx::PgPool,
-        }
-    } else if #[cfg(feature = "MysqlDB")] {
-        pub struct Storage {
-            pool: sqlx::MySqlPool,
-        }
-    }
+#[cfg(any(
+    not(any(feature = "any_db", feature = "postgres_db", feature = "mysql_db")),
+    all(feature = "any_db", feature = "postgres_db", feature = "mysql_db"),
+    all(feature = "any_db", feature = "postgres_db"),
+    all(feature = "postgres_db", feature = "mysql_db"),
+    all(feature = "any_db", feature = "mysql_db")
+))]
+compile_error!("Pick any one of \"any_db\", \"postgresql_db\", \"mysql_db\" feature flags.");
+
+pub struct Storage {
+    #[cfg(feature = "any_db")]
+    pool: sqlx::AnyPool,
+    #[cfg(feature = "postgres_db")]
+    pool: sqlx::PgPool,
+    #[cfg(feature = "mysql_db")]
+    pool: sqlx::MySqlPool
 }
 
 impl Storage {
@@ -26,6 +28,17 @@ impl Storage {
         let pool = get_db_pool().await;
         Storage { pool }
     }
+}
+
+// #[async_trait]
+// pub trait MediatorPersistence {
+//     async fn persist_forward_message(&self, forward_msg: &ForwardMsg);
+// }
+
+// #[async_trait]
+// impl MediatorPersistence for Storage {
+
+impl Storage {
     pub async fn persist_forward_message(&self, forward_msg: &ForwardMsg) {
         sqlx::query("INSERT INTO forward_raw VALUES (DEFAULT, ?, ?, DEFAULT)")
             .bind(&forward_msg.recipient_key)
