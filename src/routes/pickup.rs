d593270bf55e8181608e4eac6b16f1c3bdd8525b
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-07-26 16:04:26 +0200

    chore(refactor): modularize handle_pickup

--- src/routes/pickup.rs
+++ src/routes/pickup.rs
@@ -1,30 +1,53 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
-use crate::didcomm_types::PickupMsgEnum;
-use crate::{didcomm_types::PickupStatusMsg, storage::MediatorPersistence};
+use crate::didcomm_types::{PickupMsgEnum, PickupStatusMsg, PickupStatusReqMsg};
+use crate::storage::MediatorPersistence;
 use axum::{extract::State, Json};
-use std::sync::Arc;
 use log::info;
+use std::sync::Arc;
 
 pub async fn handle_pickup<T: MediatorPersistence>(
     State(storage): State<Arc<T>>,
     Json(pickup_message): Json<PickupMsgEnum>,
 ) -> Json<PickupMsgEnum> {
-
     match &pickup_message {
         PickupMsgEnum::PickupStatusReqMsg(status_request) => {
-            info!("Received StatusReqMsg {:#?}", &status_request);
-            let message_count = storage.retrieve_pending_message_count(&status_request.recipient_key).await;
-            let status = PickupStatusMsg::new(message_count.try_into().unwrap(), &status_request.recipient_key);
-            info!("Sending StatusMsg {:#?}", &status);
-            Json(PickupMsgEnum::PickupStatusMsg(status))
+            handle_pickup_status_req(status_request, storage).await
         }
         PickupMsgEnum::PickupStatusMsg(status) => {
-            info!("Received StatusMsg {:#?}", &status);
-            let message_count = storage.retrieve_pending_message_count(&status.recipient_key).await;
-            let status = PickupStatusMsg::new(message_count.try_into().unwrap(), &status.recipient_key);
-            info!("Sending StatusMsg {:#?}", &status);
-            Json(PickupMsgEnum::PickupStatusMsg(status))
+            handle_pickup_status(status, storage).await
         }
     }
+}
+
+async fn handle_pickup_status_req<T: MediatorPersistence>(
+    status_request: &PickupStatusReqMsg,
+    storage: Arc<T>,
+) -> Json<PickupMsgEnum> {
+    info!("Received {:#?}", &status_request);
+    let message_count = storage
+        .retrieve_pending_message_count(&status_request.recipient_key)
+        .await;
+    let status = PickupStatusMsg::new(
+        message_count.try_into().unwrap(),
+        &status_request.recipient_key,
+    );
+    info!("Sending {:#?}", &status);
+    Json(PickupMsgEnum::PickupStatusMsg(status))
+}
+
+async fn handle_pickup_status<T: MediatorPersistence>(
+    status: &PickupStatusMsg,
+    storage: Arc<T>
+) -> Json<PickupMsgEnum> {
+    info!("Received {:#?}", &status);
+    let message_count = storage
+        .retrieve_pending_message_count(&status.recipient_key)
+        .await;
+    let status = PickupStatusMsg::new(
+        message_count.try_into().unwrap(),
+        &status.recipient_key
+    );
+    info!("Sending {:#?}", &status);
+    Json(PickupMsgEnum::PickupStatusMsg(status))
 }
--- src/server.rs
+++ src/server.rs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 use crate::router::create_router;
 use crate::storage;
+use log::info;
 
 pub async fn run_server() {
     // Initialize a storage struct (this handles connection to storage backend)
     let storage = storage::init().await;
     // app definition, and routings
     let app = create_router(storage);
-
+    info!("Starting server");
     // add server task to main loop
     axum::Server::bind(&"127.0.0.1:7999".parse().unwrap())
         .serve(app.into_make_service())
