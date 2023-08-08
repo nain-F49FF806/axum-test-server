ee6456b32a8c57d4cab1cb17d808bd162fc277c7
nain <126972030+nain-F49FF806@users.noreply.github.com>
2023-08-08 12:36:00 +0200

    tes(forward)t: setup account before testing forward message persistence

--- tests/e2e-persistence.rs
+++ tests/e2e-persistence.rs
@@ -1,20 +1,48 @@
 // Copyright 2023 Naian G.
 // SPDX-License-Identifier: Apache-2.0
 
-use xum_test_server::didcomm_types::ForwardMsg;
+use serde_json::json;
+use std::sync::Once;
+static INIT: Once = Once::new();
 
 const BASE_URL: &str = "http://localhost:7999";
 
 // Test variables
 const RECIPIENT_KEY: &str = "Anderson Smith n0r3t1";
-const HELLO_ALICE: &str = "Hello Alice!";
+
+fn setup_account() {
+    let client = reqwest::blocking::Client::new();
+    let endpoint = format!("{BASE_URL}/coord");
+    let new_account_req = json!(
+        {
+            "@type": "https://didcomm.org/coordinate-mediation/1.0/mediate-request",
+            "auth_pubkey": RECIPIENT_KEY
+        }
+    );
+    let res = client.post(endpoint).json(&new_account_req).send().unwrap();
+    res.error_for_status().unwrap();
+}
+
+pub fn initialize() {
+    INIT.call_once(|| {
+        setup_account();
+    });
+}
 
 // #[ignore]
 #[test]
 fn test_forward_message() {
+    initialize();
     let client = reqwest::blocking::Client::new();
     let endpoint = format!("{BASE_URL}/forward");
-    let forward_message = ForwardMsg::new(RECIPIENT_KEY, HELLO_ALICE);
+    let forward_message = json!(
+        {
+            "@type" : "https://didcomm.org/routing/1.0/forward",
+            "@id": "54ad1a63-29bd-4a59-abed-1c5b1026e6fd",
+            "to"   : RECIPIENT_KEY,
+            "msg"  : "<super secret packed DIDCOMM message>"
+        }
+    );
     let res = client.post(endpoint).json(&forward_message).send().unwrap();
     res.error_for_status().unwrap();
 }
